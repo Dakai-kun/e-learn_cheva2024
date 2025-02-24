ceee81d769339670c5f9ffc8eecafdd771efc39a
DzakaAlif <dzakaalifnurhakim@smkwikrama.sch.id>
2025-02-24 16:49:54 +0700

    add script.js file

---
+++ Week3-2/script.js
@@ -0,0 +1,71 @@
+let notes = [
+    {
+        name: "Belajar HTML dan CSS",
+        description: "Memahamai dasar-dasar HTML dan CSS untuk membuat website yang menarik",
+        dl: "Deadline: 2024-12-15"
+    },
+    {
+        name: "Belajar HTML dan CSS",
+        description: "Memahamai dasar-dasar HTML dan CSS untuk membuat website yang menarik",
+        dl: "Deadline: 2024-12-15"
+    },
+    {
+        name: "Belajar HTML dan CSS",
+        description: "Memahamai dasar-dasar HTML dan CSS untuk membuat website yang menarik",
+        dl: "Deadline: 2024-12-15"
+    },
+    {
+        name: "Belajar HTML dan CSS",
+        description: "Memahamai dasar-dasar HTML dan CSS untuk membuat website yang menarik",
+        dl: "Deadline: 2024-12-15"
+    },
+    {
+        name: "Belajar HTML dan CSS",
+        description: "Memahamai dasar-dasar HTML dan CSS untuk membuat website yang menarik",
+        dl: "Deadline: 2024-12-15"
+    },
+];
+
+let notesElement = document.getElementById("notes-list")
+function renderNotes() {
+    notesElement.innerHTML = ""
+    notes.forEach((note, index) => {
+        const newElement = `
+            <div class="box">
+                <label for="" class="judul">${note.name}</label> <br>
+                <label for="" class="isi">${note.description}</label> <br>
+                <label for="" class="dl">Deadline: Deadline: ${note.dl}</label> <br>
+                <button class="hapus" onclick = "deleteNotes(${index})">Hapus</button>
+            </div>
+        `
+        notesElement.innerHTML += newElement
+    })
+}
+
+function addNote() {
+    const newName = document.getElementById("name").value
+    const newDescription = document.getElementById("description").value
+    const newDeadline = document.getElementById("dl").value
+    if (newName && newDescription && newDeadline && newName.length > 1 && newDescription.length > 1 && newDeadline.length > 1) 
+        {      
+        console.log(newName, newDescription, newDeadline)
+        const newData = {
+            name: newName,
+            description: newDescription,
+            dl: newDeadline
+        }
+        notes.push(newData)
+        renderNotes()
+    }
+}
+
+function deleteNotes(index) {
+    notes.splice(index, 1)
+    // notes.splice(index, 1, 
+    //     {title: "Data Baru 1", description: "Data Baru 1"},
+    //     {title: "Data Baru 2", description: "Data Baru 2"}
+    // )
+    renderNotes()
+}
+
+renderNotes()
